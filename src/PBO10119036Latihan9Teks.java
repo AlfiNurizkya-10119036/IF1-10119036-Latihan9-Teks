d275cca2f7800f3eabda0f203e9ad6912d58829c
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 13:12:45 +0700

    Project 9

---
+++ src/PBO10119036Latihan9Teks.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKAN DATA TEKS
+ */
+public class PBO10119036Latihan9Teks {
+
+ 
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "Berisi " + c);
+    }
+    
+}
